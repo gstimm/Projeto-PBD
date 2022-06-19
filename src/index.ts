8bee4fcf72ac052eb50f4792c49aef1af4bacf8c
gstimm <contato.gabrieltimm@gmail.com>
2022-06-19 20:49:53 -0300

    chore: add favicon

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import app from "./app";
 import dotenv from "dotenv";
 import path from "path";
+import favicon from "serve-favicon";
 
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
 });
+
+app.use(favicon(path.join(__dirname, "../public/favicon.ico")));
 
 export const PORT = process.env.PORT || 3333;
 
