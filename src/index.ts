1f67dd60b5643faf5c602d5e71265ed5ad0cdbee
gstimm <contato.gabrieltimm@gmail.com>
2022-06-19 19:11:08 -0300

    fix: build????

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import app from "./app";
+import dotenv from "dotenv";
+import path from "path";
 
-import { PORT } from "./config";
+dotenv.config({
+  path: path.resolve(__dirname, "../.env"),
+});
+
+export const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
